e0eb7d5fb7a1f78b82f3d885556898b046a0159d
kristaponis <a.kristaponis@icloud.com>
2021-02-07 20:38:16 +0200

    Added gorilla/mux router and created simple handler

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Hello</h1>"))
+}
+
 func main() {
-	
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeHandler)
+
+	fmt.Println("Running server on http://127.0.0.1:8080")
+	http.ListenAndServe(":8080", r)
 }
