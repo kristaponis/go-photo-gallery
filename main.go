0ce99ed04df4f9b2d66083ba1a39a54917562bb3
kristaponis <a.kristaponis@icloud.com>
2021-02-08 10:04:02 +0200

    Implemented simple HTTP error 404 - Not Found page

--- main.go
+++ main.go
@@ -12,10 +12,18 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello</h1>"))
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("<h3>404 Error - Not Found</h3>"))
+	w.Write([]byte("<p><a href=\"/\">Go to Home Page</a></p>"))
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", homeHandler)
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	fmt.Println("Running server on http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", r)
